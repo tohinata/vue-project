30635cccd42d885e383fbc678a856d6bd1cf4cc3
tohinata <tohinata@naver.comm>
2023-04-18 23:49:18 +0900

    데이터 바인딩 예제 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DataBinding from '../views/DataBinding.vue'
+
 
 const routes = [
   {
@@ -14,6 +16,76 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/databinding',
+    name: 'DataBinding',
+    component: DataBinding
+  },
+  {
+    path: '/databindinghtml',
+    name: 'DataBindingHtml',
+    component: () => import( /* webpackChunkName: "databindinghtml" */ '../views/DataBindingHtml.vue')
+  },
+  {
+    path: '/databindinginputtext',
+    name: 'DataBindingInputText',
+    component: () => import( /* webpackChunkName: "databindinginputtext" */ '../views/DataBindingInputText.vue')
+  },
+  {
+    path: '/databindinginputnumber',
+    name: 'DataBindingInputNumber',
+    component: () => import( /* webpackChunkName: "databindinginputnumber" */ '../views/DataBindingInputNumber.vue')
+  },
+  {
+    path: '/databindingtextarea',
+    name: 'DataBindingTextarea',
+    component: () => import( /* webpackChunkName: "databindingtextarea" */ '../views/DataBindingTextarea.vue')
+  },
+  {
+    path: '/databindingselect',
+    name: 'DataBindingSelect',
+    component: () => import( /* webpackChunkName: "databindingselect" */ '../views/DataBindingSelect.vue')
+  },
+  {
+    path: '/databindingcheckbox',
+    name: 'DataBindingCheckbox',
+    component: () => import( /* webpackChunkName: "databindingcheckbox" */ '../views/DataBindingCheckbox.vue')
+  },
+  {
+    path: '/databindingcheckbox2',
+    name: 'DataBindingCheckbox2',
+    component: () => import( /* webpackChunkName: "databindingcheckbox2" */ '../views/DataBindingCheckbox2.vue')
+  },
+  {
+    path: '/databindingradio',
+    name: 'DataBindingRadio',
+    component: () => import( /* webpackChunkName: "databindingradio" */ '../views/DataBindingRadio.vue')
+  },
+  {
+    path: '/databindingattribute',
+    name: 'DataBindingAttribute',
+    component: () => import( /* webpackChunkName: "databindingattribue" */ '../views/DataBindingAttribute.vue')
+  },
+  {
+    path: '/databindingbutton',
+    name: 'DataBindingButton',
+    component: () => import( /* webpackChunkName: "databindingbutton" */ '../views/DataBindingButton.vue')
+  },
+  {
+    path: '/databindingclass',
+    name: 'DataBindingClass',
+    component: () => import( /* webpackChunkName: "databindingclass" */ '../views/DataBindingClass.vue')
+  },
+  {
+    path: '/databindingclass2',
+    name: 'DataBindingClass2',
+    component: () => import( /* webpackChunkName: "databindingclass2" */ '../views/DataBindingClass2.vue')
+  },
+  {
+    path: '/databindingstyle',
+    name: 'DataBindingStyle',
+    component: () => import( /* webpackChunkName: "databindingstyle" */ '../views/DataBindingStyle.vue')
   }
 ]
 
