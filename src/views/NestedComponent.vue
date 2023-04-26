dc519d10dca93ae7a9d601a5403081e9f6251ddf
tohinata <tohinata@naver.comm>
2023-04-27 00:24:57 +0900

    8.1.2.1 정적/동적 Prop 전달

--- src/views/NestedComponent.vue
+++ src/views/NestedComponent.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
-        <PageTitle title="부모 컴포넌트에서 자식 컴포넌트로 데이터 전달"/>  <!-- import한 컴포넌트 이름을 이용해서 태그 만들면 됨 -->
+        <!-- 정적 Prop 전달 -->
+        <PageTitle title="정적 데이터 전달"/>
+        <!-- 동적 Prop 전달 -->
+        <PageTitle :title="this.title"/>
+        <PageTitle v-bind:title="this.title"/>
     </div>
 </template>
 
 <script>
     import PageTitle from '../components/PageTitle';  // 컴포넌트(자식) import
     export default {
-        components: {PageTitle}  // 현재 컴포넌트에서 사용할 컴포넌트 등록
+        components: {PageTitle},  // 현재 컴포넌트에서 사용할 컴포넌트 등록
+        data() {
+            return {
+                title: 'Prop에 동적 데이터 전달'
+            };
+        }
     }
+    
+
 </script>
